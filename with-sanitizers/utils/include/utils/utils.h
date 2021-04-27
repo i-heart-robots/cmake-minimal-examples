72bf3dfeaf59894753ad7312708dc861bfc3edc0
josh.redding <josh.redding@icloud.com>
2021-04-27 11:22:08 -0600

    Add sanitizers to an example

---
+++ with-sanitizers/utils/include/utils/utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace utils {
+
+// FYI: PascalCase, snake_case
+std::string PascalCaseToSnakeCase(const std::string& pascal_case);
+
+}  // namespace utils
